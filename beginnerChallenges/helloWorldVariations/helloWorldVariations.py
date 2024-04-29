3806a35d95eb00e05e2fd9ebf5f74887db0445db
AtlasRB <connoryeeles@outlook.com>
2024-04-29 17:17:25 +0100

    Added and completed beginner challenge 1 - hello world variations

---
+++ beginnerChallenges/helloWorldVariations/helloWorldVariations.py
@@ -0,0 +1,23 @@
+#Print Hello World
+print("Hello World \n")
+
+#Different Langauges
+print("English - Hello World")
+print("Spanish - Hola Mundo")
+print("French - Bonjour le monde")
+print("Japanese - Kon'nichiwa sekai \n")
+
+#Include a name
+name = input("Enter your name: ")
+print("Hello " + name + "\n")
+
+#Create a picture
+print("#   # ##### #     #       ###      #   #  ###  ####  #     ####  ")
+print("#   # #     #     #      #   #     # # # #   # #   # #     #   # ")
+print("##### ####  #     #      #   #     # # # #   # ####  #     #   # ")
+print("#   # #     #     #      #   #     # # # #   # # ##  #     #   # ")
+print("#   # ##### ##### #####   ###       # #   ###  #  ## ##### ####  \n")
+
+#Repeat the code
+for i in range(10):
+    print(str(i+1) + ". Hello World")
