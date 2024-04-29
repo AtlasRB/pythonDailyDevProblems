76749121408712c4559c53cdb32ab76d17a6a0e0
AtlasRB <connoryeeles@outlook.com>
2024-04-29 18:44:42 +0100

    Added and completed beginner challenge 2 - Sum of Two Numbers

---
+++ beginnerChallenges/sumOfTwoNumbers/sumOfTwoNumbers.py
@@ -0,0 +1,7 @@
+#Sum of Two Numbers
+number1 = int(input("Please enter your first number: "))
+number2 = int(input("Please enter your second number: "))
+
+total = number1 + number2
+
+print("The sum of", number1, "and", number2, "=", total)
